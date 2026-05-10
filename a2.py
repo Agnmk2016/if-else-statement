b719c277a6cfed8d6d048dd1b2181345019b9f8a
Agnmk2016 <komeyaiswellgigs@gmail.com>
2026-05-10 16:17:39 +0000

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,17 @@
+print("which cake would  you like to order")
+
+print("1. Chocolate cake.")
+print("2. Blueberry cheese cake.")
+print("3. Red valvet cake.")
+print("4. strawberrycake")
+
+value = input(":")
+
+if value == '1':
+    print("you have ordered Chocolate cake")
+elif value =='2':
+    print("you have ordered a Blueberry cheese cake")
+elif value == '3':
+    print("you have ordered a Red velvet cake")
+elif value == '4':
+    print("you have ordered a Strawberry cake")
